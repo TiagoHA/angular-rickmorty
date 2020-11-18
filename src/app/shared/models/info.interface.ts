1aec6b01493233d85c8c51e29da82af6cb377d47
Tiago Almeida <Tiagu.Almeida@gmail.com>
2020-11-18 02:09:05 -0200

    ✨ adding new files in share module

---
+++ src/app/shared/models/info.interface.ts
@@ -0,0 +1,6 @@
+export interface IInfo {
+  count: number
+  pages: 3
+  next: string
+  prev: string | null
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 import { MatPaginatorModule } from '@angular/material/paginator'
+import { MatTableModule } from '@angular/material/table'
+import { MatIconModule } from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatPaginatorModule],
-  exports: [MatPaginatorModule],
+  imports: [
+    CommonModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
+  exports: [
+    CommonModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
 })
 export class SharedModule {}
