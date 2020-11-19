dba34c890ade5af5cc444a5265e0ca79e8f2951d
Tiago Almeida <Tiagu.Almeida@gmail.com>
2020-11-18 23:22:52 -0200

    ✨ adding dynamic buttons in header

---
+++ src/app/shared/components/header/header.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnDestroy, OnInit } from '@angular/core'
+import { NavigationEnd, Router } from '@angular/router'
+import { Subscription } from 'rxjs'
+
+@Component({
+  selector: 'spa-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+})
+export class HeaderComponent implements OnInit, OnDestroy {
+  public url = ''
+
+  public firstButton
+  public secondButton
+  public buttons = []
+  private subs: Subscription
+
+  constructor(private router: Router) {}
+  ngOnInit(): void {
+    this.subs = this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log('this.router.url', this.router.url)
+
+        this.url = this.router.url
+        this.updatingLinks(this.router.url)
+      }
+    })
+  }
+
+  ngOnDestroy() {
+    this.subs?.unsubscribe()
+  }
+
+  updatingLinks(link: string) {
+    const urls = [
+      { name: 'Episodes', url: '/episodes' },
+      { name: 'Characters', url: '/characters' },
+      { name: 'Locations', url: '/locations' },
+    ]
+
+    this.buttons = urls
+      .filter((url) => url.url !== link)
+      .sort((a, b) => a.name.localeCompare(b.name))
+    ;[this.firstButton, this.secondButton] = this.buttons
+  }
+}
